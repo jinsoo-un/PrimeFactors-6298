d6f1b40571e2bbcab2dc2588e560f7a18ee7cc08
see.jin <bobstar0@naver.com>
2025-07-02 10:14:47 +0900

    of3

--- PrimeFactorKATA/prime-factors_test.cpp
+++ PrimeFactorKATA/prime-factors_test.cpp
@@ -1,14 +1,25 @@
 #include <gmock/gmock.h>
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+
+class PrimeFixture : public Test {
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFixture, Of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
