3555be84160226c5b8485ab94d027738bd49a355
see.jin <bobstar0@naver.com>
2025-07-02 10:21:54 +0900

    of6

--- PrimeFactorKATA/prime-factors.cpp
+++ PrimeFactorKATA/prime-factors.cpp
@@ -9,8 +9,14 @@ public:
 
 		if (number > 1) {
 			if (number == 4) {
+				while( number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}
+			}
+			else if (number == 6) {
 				result.push_back(2);
-				result.push_back(2);
+				result.push_back(3);
 			}
 			else {
 				result.push_back(number);
