00fec7b27bc1ecf3c8abb9c3d73dba237eb0ee70
see.jin <bobstar0@naver.com>
2025-07-02 10:09:20 +0900

    of1

--- PrimeFactorKATA/main.cpp
+++ PrimeFactorKATA/main.cpp
@@ -1,5 +1,12 @@
 #include <gmock/gmock.h>
 
+TEST (PrimeFactorTest, PrimeTest) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
---
+++ PrimeFactorKATA/prime-factors.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using std::vector;
+
+class PrimeFactors {
+public:
+	vector<int> of(int number) {
+		vector<int> result = {};
+		return result;
+
+		//if (number < 2) {
+		//	return 0; // No prime factors for numbers less than 2
+		//}
+		//int factor = 2;
+		//while (number > 1) {
+		//	if (number % factor == 0) {
+		//		number /= factor;
+		//	} else {
+		//		factor++;
+		//	}
+		//}
+		//return factor; // Return the last found prime factor
+	}
+private:
+
+};
